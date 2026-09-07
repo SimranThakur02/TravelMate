fd134aa85652060b8500b2d7da735bde3956a309
SimranThakur02 <simranthakur99749@gmail.com>
2026-09-07 20:07:05 +0530

    change4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Destinations from "../components/Destinationss";
 import Modules from "../components/Modules";
-import WhyTravelMate from "../components/WhytravelMate";
+import WhyTravelMate from "../components/Whytravelmate";
 import Footer from "../components/Footer";
 
 function Home() {
